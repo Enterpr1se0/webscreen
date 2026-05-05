ee4b2002c6ce3793e5d5a0d13800084b030e5b0c
hiroi <dukihiroi@gmail.com>
2026-05-05 19:33:19 +0900

    add log

--- webservice/handleScreen.go
+++ webservice/handleScreen.go
@@ -45,6 +45,7 @@ func (wm *WebMaster) handleScreenWS(c *gin.Context) {
 		conn.Close()
 		return
 	}
+	log.Printf("Received connection config: %+v", config)
 
 	// Create a unique ID for one abstract device
 	deviceIdentifier := config.DeviceType + "_" + config.DeviceID + "_" + config.DeviceIP + "_" + config.DevicePort
